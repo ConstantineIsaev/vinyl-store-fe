13a21a1358a6f4369379eb7dbb5d7e71c67e258b
Isaev <matthew.riesdale@gmail.com>
2020-11-17 05:32:24 +0300

    link to /import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   products: 'https://0ahpp7egsj.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://bym0s36ip7.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
