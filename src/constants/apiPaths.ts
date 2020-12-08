531a414c09ac2b2875164330df6e30cfa5260e0e
Isaev <matthew.riesdale@gmail.com>
2020-12-08 03:41:13 +0300

    update cart component

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   products: 'https://0ahpp7egsj.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://bym0s36ip7.execute-api.us-east-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://constantineisaev-rs-cart-api-dev.us-east-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
